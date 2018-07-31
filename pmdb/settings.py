302e5ff20f64a1e1b06c8ebf5554a01e530846d1
Adrian Jasinski <jasinski.adrian@gmail.com>
2018-07-31 16:26:36 +0200

    add new aplication to settings and requirements

--- pmdb/settings.py
+++ pmdb/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # development
+    'django_extensions',
+    # projects
+    'pmdb',
+    'pipeline',
+    # other
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
