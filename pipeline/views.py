9fad2c40514d224d0438347d686e6cb333d690c0
Adrian Jasinski <jasinski.adrian@gmail.com>
2018-07-31 16:24:44 +0200

    add DetailView

--- pipeline/views.py
+++ pipeline/views.py
@@ -1,3 +1,10 @@
+from django.views.generic import DetailView
 from django.shortcuts import render
 
-# Create your views here.
+from pipeline.models import Project
+
+
+
+class ProjectDetailView(DetailView):
+    model = Project
+    template_name = 'pipeline/project_details.html'
