734ecbb4efea3a68ef2da3793c1ecd66501694fb
Adrian Jasinski <jasinski.adrian@gmail.com>
2018-08-07 16:42:09 +0200

    add new models for selection options

--- dropdowns/models.py
+++ dropdowns/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Molecule(models.Model):
+    """Database model for API molecule."""
+
+    name = models.CharField(max_length=300, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+class PharmaForm(models.Model):
+    """Pharmaceutical form of Molecule."""
+
+    type = models.CharField(max_length=300, null=False, blank=False)
+
+    def __str__(self):
+        return self.type
+
+class TherapeuticArea(models.Model):
+    """List of all Therapeutic Areas."""
+
+    type = models.CharField(max_length=300, null=False, blank=False)
+
+    def __str__(self):
+        return self.type
+
+class PackType(models.Model):
+    """Django data model PackType"""
+
+    type = models.CharField(max_length=300, null=False, blank=False)
+
+    def __str__(self):
+        return self.type
+
+class AtcClass(models.Model):
+    """Django data model AtcClass"""
+
+    code = models.CharField(max_length=6, null=False, blank=False)
+    type = models.CharField(max_length=300, null=False, blank=False)
+
+    def __str__(self):
+        return str(f"{self.code} {self.type}")
