62f1a21fc7b2c0dc69ecbabf26812e6e8ed392d8
Adrian Jasinski <jasinski.adrian@gmail.com>
2018-08-09 15:26:28 +0200

    create Market model

--- dropdowns/models.py
+++ dropdowns/models.py
@@ -7,6 +7,10 @@ class Molecule(models.Model):
 
     name = models.CharField(max_length=300, null=False, blank=False)
 
+    class Meta:
+        verbose_name = 'Molecule'
+        verbose_name_plural = 'Molecules'
+
     def __str__(self):
         return self.name
 
@@ -15,6 +19,10 @@ class PharmaForm(models.Model):
     """Pharmaceutical form of Molecule."""
 
     type = models.CharField(max_length=300, null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Pharmaceutical form'
+        verbose_name_plural = 'Pharmaceutical forms'
 
     def __str__(self):
         return self.type
@@ -25,6 +33,10 @@ class TherapeuticArea(models.Model):
 
     type = models.CharField(max_length=300, null=False, blank=False)
 
+    class Meta:
+        verbose_name = 'Therapeutic area'
+        verbose_name_plural = 'Therapeutic areas'
+
     def __str__(self):
         return self.type
 
@@ -33,6 +45,10 @@ class PackType(models.Model):
     """Django data model PackType"""
 
     type = models.CharField(max_length=300, null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Pack type'
+        verbose_name_plural = 'Pack types'
 
     def __str__(self):
         return self.type
@@ -44,6 +60,10 @@ class AtcClass(models.Model):
     code = models.CharField(max_length=6, null=False, blank=False)
     type = models.CharField(max_length=300, null=False, blank=False)
 
+    class Meta:
+        verbose_name = 'ATC-3 Class'
+        verbose_name_plural = 'ATC-3 Classes'
+
     def __str__(self):
         return str(f"{self.code} {self.type}")
 
@@ -52,6 +72,10 @@ class Market(models.Model):
     """Django data model Market"""
 
     name = models.CharField(max_length=300, null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Market'
+        verbose_name_plural = 'Markets'
 
     def __str__(self):
         return str(self.name)
