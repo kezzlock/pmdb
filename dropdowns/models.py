f743f222a0d0256d652eb7b877fa789992dc9d5e
Adrian Jasinski <jasinski.adrian@gmail.com>
2018-08-23 09:18:42 +0200

    sort models alphabet order

--- dropdowns/models.py
+++ dropdowns/models.py
@@ -2,6 +2,56 @@ import pycountry
 from django.db import models
 
 
+class AtcClass(models.Model):
+    """Django data model AtcClass"""
+
+    code = models.CharField(max_length=6, null=False, blank=False)
+    type = models.CharField(max_length=300, null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'ATC-3 Class'
+        verbose_name_plural = 'ATC-3 Classes'
+
+    def __str__(self):
+        return str(f"{self.code} {self.type}")
+
+
+class Licensor(models.Model):
+    """Django data model Licensors"""
+
+    name = models.CharField(max_length=300, null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Licensor'
+        verbose_name_plural = 'Licensors'
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Market(models.Model):
+    """Django data model Market"""
+
+    name = models.CharField(max_length=300, null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Market'
+        verbose_name_plural = 'Markets'
+
+    def __str__(self):
+        return str(self.name)
+
+    # use https://pypi.org/project/pycountry/ for auto code generation
+    @property
+    def code(self):
+        try:
+            country = pycountry.countries.get(name=self.name)
+            return country.alpha2
+        except Exception:
+            country = ''
+        return country
+
+
 class Molecule(models.Model):
     """Database model for API molecule."""
 
@@ -13,6 +63,19 @@ class Molecule(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class PackType(models.Model):
+    """Django data model PackType"""
+
+    type = models.CharField(max_length=300, null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Pack type'
+        verbose_name_plural = 'Pack types'
+
+    def __str__(self):
+        return self.type
 
 
 class PharmaForm(models.Model):
@@ -39,65 +102,3 @@ class TherapeuticArea(models.Model):
 
     def __str__(self):
         return self.type
-
-
-class PackType(models.Model):
-    """Django data model PackType"""
-
-    type = models.CharField(max_length=300, null=False, blank=False)
-
-    class Meta:
-        verbose_name = 'Pack type'
-        verbose_name_plural = 'Pack types'
-
-    def __str__(self):
-        return self.type
-
-
-class AtcClass(models.Model):
-    """Django data model AtcClass"""
-
-    code = models.CharField(max_length=6, null=False, blank=False)
-    type = models.CharField(max_length=300, null=False, blank=False)
-
-    class Meta:
-        verbose_name = 'ATC-3 Class'
-        verbose_name_plural = 'ATC-3 Classes'
-
-    def __str__(self):
-        return str(f"{self.code} {self.type}")
-
-
-class Market(models.Model):
-    """Django data model Market"""
-
-    name = models.CharField(max_length=300, null=False, blank=False)
-
-    class Meta:
-        verbose_name = 'Market'
-        verbose_name_plural = 'Markets'
-
-    def __str__(self):
-        return str(self.name)
-
-    # use https://pypi.org/project/pycountry/ for auto code generation
-    @property
-    def code(self):
-        try:
-            country = pycountry.countries.get(name=self.name)
-            return country.alpha2
-        except Exception:
-            country = ''
-        return country
-
-class Licensors(models.Model):
-    """Django data model Licensors"""
-
-    name = models.CharField(max_length=300, null=False, blank=False)
-
-    class Meta:
-        verbose_name = 'Licensors'
-        verbose_name_plural = 'Licensors'
-
-    def __str__(self):
-        return str(self.id)
