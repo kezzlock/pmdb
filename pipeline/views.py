208e8c37fbbd2bb2a74be40546ae5dadb5b23f25
Adrian Jasinski <jasinski.adrian@gmail.com>
2018-09-11 15:20:01 +0200

    columns generations for javascript (commented)

--- pipeline/views.py
+++ pipeline/views.py
@@ -1,3 +1,6 @@
+import json
+import re
+
 from django.urls import reverse_lazy
 from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
                                   UpdateView)
@@ -9,6 +12,24 @@ from pipeline.models import Project
 class ProjectListView(ListView):
     model = Project
     template_name = 'pipeline/project_list.html'
+    # columns = ['id', 'name', 'molecule', 'form', 'strength', 'brand_name',
+    #            'market', 'moq']
+
+    # def get_context_data(self, **kwargs):
+    #     # Call the base implementation first to get a context
+    #     context = super().get_context_data(**kwargs)
+    #     # Prepare list to create JSON
+    #     columns_json = []
+    #     for column in self.columns:
+    #         temp = {}
+    #         temp['data'] = column
+    #         temp['title'] = ' '.join(word.title() for word in filter(
+    #             None, re.split("[, \-!?:_]+", column)))
+    #         columns_json.append(temp)
+    #     json_dumps = json.dumps(columns_json)
+    #     print(json_dumps.replace('{\"', '{').replace('\":', ':').replace('"t', 't').replace('"','\''))
+    #     context['columns_json'] = json_dumps.replace('\"', '')
+    #     return context
 
 
 class ProjectDetailView(DetailView):
