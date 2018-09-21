4949753df4dc2371e0f07513b9d9f70cda54cec0
Adrian Jasinski <jasinski.adrian@gmail.com>
2018-09-21 11:17:12 +0200

    add method for verbose fields

--- pmdb/templatetags/verbose_tags.py
+++ pmdb/templatetags/verbose_tags.py
@@ -11,3 +11,10 @@ def verbose_name(obj):
 @register.filter
 def verbose_name_plural(obj):
     return obj._meta.verbose_name_plural
+
+@register.simple_tag
+def get_verbose_field_name(instance, field_name):
+    """
+    Returns verbose_name for a field.
+    """
+    return instance._meta.get_field(field_name).verbose_name.title()
