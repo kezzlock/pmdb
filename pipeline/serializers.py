9bce3e80e1ef6db42f938a5e96aadcacffb22530
Adrian Jasinski <jasinski.adrian@gmail.com>
2018-09-28 15:37:34 +0200

    add method for hendling integer choices in serializer

--- pipeline/serializers.py
+++ pipeline/serializers.py
@@ -4,6 +4,15 @@ from pipeline.models import Project
 
 
 class ProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields = '__all__'
+    # TODO: create autointerator for integer fields
+     project_type = serializers.CharField(source='get_project_type_display')
+     contract_type = serializers.CharField(source='get_contract_type_display')
+     status = serializers.CharField(source='get_status_display')
+     prescription_category = serializers.CharField(source='get_prescription_category_display')
+     priority = serializers.CharField(source='get_priority_display')
+     shelf_life = serializers.CharField(source='get_shelf_life_display')
+     risk_type = serializers.CharField(source='get_risk_type_display')
+
+     class Meta:
+         model = Project
+         fields = '__all__'
