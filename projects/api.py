08e04937cf9f2d271878d58b887b598617a55a54
Adrian Jasinski <jasinski.adrian@gmail.com>
2018-10-03 15:54:29 +0200

    add method to filter search queries from tables search field

--- projects/api.py
+++ projects/api.py
@@ -1,9 +1,29 @@
+import re
+
+from django.db.models import CharField, ForeignKey, Q, TextField
 from django.utils.html import escape
 from django_datatables_view.base_datatable_view import BaseDatatableView
 from rest_framework import generics
 
 from projects.models import Project
 from projects.serializers import ProjectSerializer
+
+
+def normalize_query(query_string,
+                    findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
+                    normspace=re.compile(r'\s{2,}').sub):
+    """Split query string.
+
+    Splits the query string in invidual keywords, getting rid of unecessary
+    spaces and grouping quoted words together.
+
+    Example:
+
+        >>> normalize_query('some random  words "with   quotes  " and  spaces')
+        ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
+    """
+    return [normspace(' ', (t[0] or t[1]).strip())
+            for t in findterms(query_string)]
 
 
 class ProjectListJson(BaseDatatableView):
@@ -14,7 +34,7 @@ class ProjectListJson(BaseDatatableView):
     # 'status', 'prescription_category', 'therapeutic_area',
     # 'priority', 'atc_class', 'pack_size', 'pact_type', 'shelf_life',]
     order_columns = ['name', 'molecule', 'form',
-                     'strength', 'brand_name', 'market']
+                     'strength', 'brand_name', 'market', 'moq']
 
     def render_column(self, row, column):
         """ Renders a column on a row.
@@ -45,6 +65,54 @@ class ProjectListJson(BaseDatatableView):
             # return '<a href="%s" class="table__name-link">%s</a>' % (obj.get_absolute_url(), value)
             return f'<a href="#" class="table__name-link" onclick="showDetails({obj.pk})">{value}</a>'
         return value
+
+    def filter_queryset(self, qs):
+        # use parameters passed in GET request to filter queryset
+        try:
+            search = self.request.GET.get('search[value]', None).strip()
+        except:
+            search = ""
+        if search:
+            # normalize query string and get the list of words
+            terms = normalize_query(search)
+            # get list of text fields
+            fields = [f for f in self.model._meta.fields if isinstance(
+                f, (CharField, TextField))]
+            # get the list of foreig fields
+            foreign_fields = [
+                f for f in self.model._meta.fields if isinstance(f, ForeignKey)]
+            # get dict with ForeigKey field as key and list of Char/Text fields
+            # that are in related model as a argument
+            foreign_models_fields = {}
+            for foreign_field in foreign_fields:
+                try:
+                    fmodel = self.model._meta.get_field(
+                        '%s' % foreign_field.name).remote_field.model
+                except:
+                    # for older version of django
+                    fmodel = self.model._meta.get_field(
+                        '%s' % foreign_field.name).rel.to
+
+                foreign_models_fields[foreign_field] = [
+                    f for f in fmodel._meta.fields if isinstance(f, (CharField, TextField))]
+            # define empty Q object
+            qobjects = Q()
+            for term in terms:
+                # get queries for Char/Text fields
+                queries = [Q(**{'%s__icontains' % f.name: term})
+                           for f in fields]
+                # exted queries by foreign fields
+                if foreign_models_fields:
+                    for foreign_field, model_fields in foreign_models_fields.items():
+                        queries += [Q(**{'%s__%s__icontains' % (foreign_field.name, f.name): term})
+                                    for f in model_fields]
+
+                # crete OR statement for Q objects
+                for query in queries:
+                    qobjects = qobjects | query
+            # return filtered querystring
+            return qs.filter(qobjects)
+        return qs
 
 
 class ProjectDetailJson(generics.RetrieveUpdateDestroyAPIView):
