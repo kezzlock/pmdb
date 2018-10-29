f182f1060fb79ea61f26b361830d057a8d5f79e4
Adrian Jasinski <jasinski.adrian@gmail.com>
2018-10-29 12:59:32 +0100

    improve create form layout

--- projects/forms.py
+++ projects/forms.py
@@ -1,6 +1,6 @@
 from crispy_forms.bootstrap import FormActions
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit
+from crispy_forms.layout import Div, Layout, Submit
 from django import forms
 from django.urls import reverse_lazy
 
@@ -43,27 +43,30 @@ class ProjectCreateFormJson(forms.ModelForm):
         super(ProjectCreateFormJson, self).__init__(*args, **kwargs)
         helper = FormHelper(self)
         helper.form_class = 'manipulate-form'
+        helper.form_id = 'create_form'
         helper.form_action = reverse_lazy('project_create_json')
         helper.layout.append(
-            Submit('save_changes', 'Save changes',
-                   css_class="btn btn-block manipulate-form__submit-btn",  onclick="showmms();"))
-        # helper.disable_csrf = True
+            Div(Div(
+                    Submit('create_project', 'Create project',
+                       css_class="btn btn-block manipulate-form__submit-btn"),
+                    css_class='col-6 offset-6'),
+                css_class='row')
+            )
         self.helper = helper
 
     class Meta:
         model = Project
-        fields = ('name', )
-        # fields = ('name', 'brand_name', 'molecule', 'description',
-        #           'product_category',
-        #           'strength',
-        #           'market',
-        #           'manager',
-        #           'project_type',
-        #           'pharmaceutical_form',
-        #           'form',
-        #           'status',
-        #           'prescription_category',
-        #           'therapeutic_area',
-        #           'priority',
-        #           'atc3_class',
-        #           'otc_atc2_class')
+        fields = ('name', 'brand_name', 'molecule', 'description',
+                  'product_category',
+                  'strength',
+                  'market',
+                  'manager',
+                  'project_type',
+                  'pharmaceutical_form',
+                  'form',
+                  'status',
+                  'prescription_category',
+                  'therapeutic_area',
+                  'priority',
+                  'atc3_class',
+                  'otc_atc2_class')
